3329562b81708b97aabc63e6c209324200f05ddd
Masnoon Nayem Khan <61927060+TheGhaplaMan@users.noreply.github.com>
2021-03-14 20:08:06 +0600

    till vid 30, folder 2

--- trying_again/lib/main.dart
+++ trying_again/lib/main.dart
@@ -1,19 +1,61 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
 //void main() {
 //  runApp(MyApp());
 //}
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _queIndex = 0;
+  void answerQ() {
+    setState(() {
+      _queIndex = _queIndex + 1;
+    });
+    print(_queIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var que = [
+      "What is your fav color?",
+      "What is your fav animal?",
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("FIrst try lol"),
+          title: Text("First try lol"),
         ),
-        body: Text("Some text xD"),
+        body: Column(
+          children: [
+            Question(
+              que[_queIndex],
+            ),
+            ElevatedButton(
+              child: Text("RED"),
+              onPressed: answerQ,
+            ),
+            ElevatedButton(
+              child: Text("GREEN"),
+              onPressed: answerQ,
+            ),
+            ElevatedButton(
+              child: Text("BLUE"),
+              onPressed: () {
+                //print
+                print("Answer 3 chosen");
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
