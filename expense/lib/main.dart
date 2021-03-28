ad9e7fcc6620ebcd819b4d8962511d55cc545ecb
Masnoon Nayem Khan <61927060+TheGhaplaMan@users.noreply.github.com>
2021-03-28 21:32:21 +0600

    Folder 4 vid 11 done

--- expense/lib/main.dart
+++ expense/lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,11 +15,26 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transaction = [
+    Transaction(
+      id: "t1",
+      title: 'biri',
+      amount: 15.11,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: 'chaa',
+      amount: 5,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.redAccent,
           title: Text('Expense Check'),
         ),
         body: Column(
@@ -35,9 +50,43 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Card(
-              color: Colors.cyan,
-              child: Text("List of Transactions"),
+            Column(
+              children: transaction.map((tx) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          tx.amount.toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.white,
+                            backgroundColor: Colors.black54,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text(tx.title),
+                          Text(tx.date.toString()),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
             )
           ],
         ));
